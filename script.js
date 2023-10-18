df83a15de2a3afe9bf665fb95c8f34ce2f18deec
surazbissoyi <surazbissoyi>
2023-10-18 15:46:21 +0530

    Added Filter

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // Form sumbit event
 form.addEventListener('submit', addItem);
 
 // Delete event 
 itemList.addEventListener('click', removeItem);
+
+// Filter event
+filter.addEventListener('click', filterItems);
 
 // add item
 function addItem(e) {
@@ -45,4 +49,21 @@ function removeItem(e) {
             itemList.removeChild(li);
         }
     }
+}
+
+// Filter items
+function filterItems(e) {
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // convert to an array
+    Array.from(items).forEach(function(item) {
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
 }
